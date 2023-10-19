d175ea697bf9f8a6034a17e0c994768e731f5075
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-10-19 13:55:53 +0200

    add relation for doctorprofile with user

--- backend/app/Models/DoctorProfile.php
+++ backend/app/Models/DoctorProfile.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class DoctorProfile extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'description', 'curriculum_vitae', 'photo', 'address', 'visible'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
