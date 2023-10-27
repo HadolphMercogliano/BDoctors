9e1b59aa17782244ca240476d19a3cbc77b6cdf3
alessandroiero <alessandro.iero@outlook.it>
2023-10-27 17:58:03 +0200

    Router Installato

---
+++ frontend/src/router.js
@@ -0,0 +1,24 @@
+import {createRouter, createWebHistory} from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        // {
+        //     path: '',
+        //     name: '',
+        //     component: 
+        // },
+        // {
+        //     path: '',
+        //     name: '',
+        //     component: 
+        // },
+        { 
+            // path: '/:pathMatch(.*)*', 
+            // name: 'not-found',
+            // component: PageNotFound
+        }
+    ]
+});
+
+export { router };
